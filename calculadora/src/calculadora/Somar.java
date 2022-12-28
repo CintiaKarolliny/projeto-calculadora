a82cc04571c0b2d32258fa1ac12b0cd3ccf9d6e4
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2022-12-28 03:56:00 -0300

    :ok_hand: ci: Alteração na exibição do valor resultante e remoção de comentários

--- calculadora/src/calculadora/Somar.java
+++ calculadora/src/calculadora/Somar.java
@@ -4,21 +4,18 @@ public class Somar {
 
     private float num1;
     private float num2;
-//    private float total;
 
-    public Somar(float num1, float num2/*, float total */) {
+    public Somar(float num1, float num2) {
 
         this.num1 = num1;
         this.num2 = num2;
-       // this.total = total;
-
     }
 
     public void calcular() {
 
         float total = num1+num2;
 
-        System.out.println(num1 + " + " + num2 + " = " + total);
+        System.out.println("\n" + num1 + " + " + num2 + " = " + Math.round(total * 100.0) / 100.0);
 
     }
 
