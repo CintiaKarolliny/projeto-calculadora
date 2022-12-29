18ef2063450b24833d6ddc99e64f51e93e8b40c6
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2022-12-29 14:23:34 -0300

    :ok_hand: ci: Inclusão do método main

--- calculadora/src/calculadora/tela.java
+++ calculadora/src/calculadora/tela.java
@@ -1,21 +1,47 @@
 package calculadora;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 public class tela {
-    protected JPanel panel1;
+    private JPanel panel1;
     private JButton button1;
+    private JTextField textField1;
 
     public tela() {
 
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"teste");
-            }
-        });
+        button1.addActionListener(e -> JOptionPane.showMessageDialog(null, "teste"));
+    }
+        public static void main(String[] args) {
+
+            JFrame frame = new JFrame("tela");
+            frame.setContentPane(new tela().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.pack();
+            frame.setVisible(true);
+
+            Scanner dig = new Scanner(System.in);
+
+            System.out.print("\nDigite o 1º número: ");
+            float num1 = dig.nextFloat();
+
+            System.out.print("\nDigite o 2º número: ");
+            float num2 = dig.nextFloat();
+
+            Somar s = new Somar(num1,num2);
+
+            Subtrair sub = new Subtrair(num1,num2);
+
+            Multiplicar mult = new Multiplicar(num1,num2);
+
+            Dividir div = new Dividir(num1, num2);
+
+            s.calcular();
+            sub.calcular();
+            mult.calcular();
+            div.calcular();
+        }
 
     }
-}
+
