b97540de17b94da995383a58b06d9bddd47456aa
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2023-01-03 21:47:56 -0300

    :bug: fix: Conciliação do .form com .java na linha 82 e remoção de redundâncias

--- calculadora/src/calculadora/tela.java
+++ calculadora/src/calculadora/tela.java
@@ -5,65 +5,92 @@ import java.awt.*;
 
 public class tela {
 
-    private JFrame frame;
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton button1;
-    private JButton button2;
+    private JPanel calc;
+    private JTextField resultTextField;
+    private JButton cButton;
+    private JButton xButton;
+    private JButton parenButton;
+    private JButton porcButton;
+    private JButton divButton;
+    private JButton seteButton;
+    private JButton oitoButton;
+    private JButton noveButton;
+    private JButton quatroButton;
+    private JButton cincoButton;
+    private JButton seisButton;
+    private JButton umButton;
+    private JButton doisButton;
+    private JButton tresButton;
+    private JButton mudarSinalButton;
+    private JButton zeroButton;
+    private JButton virgulaButton;
+    private JButton igualButton;
+    private JButton maisButton;
+    private JButton menosButton;
 
     public tela() {
-        iniciar();
-    }
-    public void iniciar() {
-        frame = new JFrame();
-        frame.setBounds(100,100,300,400);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
 
+//
+////                    public static void main(String[] args) {
+////
+////                        JFrame frame = new JFrame("tela");
+////                        frame.setContentPane(new tela().calc);
+////                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+////
+////                        frame.pack();
+////                        frame.setVisible(true);
+////
+////                        Scanner dig = new Scanner(System.in);
+////
+////                        System.out.print("\nDigite o 1º número: ");
+////                        float num1 = dig.nextFloat();
+////
+////                        System.out.print("\nDigite o 2º número: ");
+////                        float num2 = dig.nextFloat();
+////
+////                        Somar s = new Somar(num1,num2);
+////
+////                        Subtrair sub = new Subtrair(num1,num2);
+////
+////                        Multiplicar mult = new Multiplicar(num1,num2);
+////
+////                        Dividir div = new Dividir(num1, num2);
+////
+////                        s.calcular();
+////                        sub.calcular();
+////                        mult.calcular();
+////                        div.calcular();
+////                    }
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        });
+
+        umButton.addActionListener(e -> {
+
+            String umButtonText = umButton.getText();
+            resultTextField.setText(umButtonText);
+        });
+
+    }
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    tela Window = new tela();
-                    Window.frame.setVisible(true);
 
-//                    public static void main(String[] args) {
-//
-//                        JFrame frame = new JFrame("tela");
-//                        frame.setContentPane(new tela().panel1);
-//                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//                        frame.pack();
-//                        frame.setVisible(true);
-//
-//                        Scanner dig = new Scanner(System.in);
-//
-//                        System.out.print("\nDigite o 1º número: ");
-//                        float num1 = dig.nextFloat();
-//
-//                        System.out.print("\nDigite o 2º número: ");
-//                        float num2 = dig.nextFloat();
-//
-//                        Somar s = new Somar(num1,num2);
-//
-//                        Subtrair sub = new Subtrair(num1,num2);
-//
-//                        Multiplicar mult = new Multiplicar(num1,num2);
-//
-//                        Dividir div = new Dividir(num1, num2);
-//
-//                        s.calcular();
-//                        sub.calcular();
-//                        mult.calcular();
-//                        div.calcular();
-//                    }
+        EventQueue.invokeLater(() -> {
+            try {
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                JFrame frame = new JFrame("calc");
+                frame.setContentPane(new tela().calc);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+                frame.setBounds(100,100,300,400);
+                frame.setResizable(false);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
+
     }
 }
